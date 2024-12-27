4fcbdb4ac9da60ea3f6205a673f02aa1916e73aa
Jazee6 <Jazee6@users.noreply.github.com>
2024-12-27 13:59:10 +0000

    deploy: Jazee6/astro-blur@794df04562e09ba2035a3cca733fa88a004a15d3

---
+++ _astro/PageLayout.astro_astro_type_script_index_0_lang.BZg9JLf0.js
@@ -0,0 +1,1 @@
+import{s as o}from"./config.BZVota3l.js";document.querySelector("#comment")!==null&&window.twikoo.init({envId:o.twikoo_uri,el:"#comment",lang:navigator.language});
--- _astro/config.DPl4AVvd.js
+++
@@ -1,1 +0,0 @@
-const e={title:"Hi! Jazee",language:"zh",description:"Jazee's personal blog. Powered by Astro Blog Theme Blur.",keywords:"Jazee, blog, personal blog, Astro, Astro Blog Theme Blur",author:"Jazee",avatar:"/avatar.png",favicon:"/favicon.png",site:"https://jaze.top",page_size:10,twikoo_uri:"https://c.blog.jaze.top/.netlify/functions/twikoo"};export{e as s};
